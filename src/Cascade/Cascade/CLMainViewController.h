886934958f95e95c2e9de6344175da657ab9fec1
Thomas Schedler <thomas@chirimoya.at>
2011-11-01 14:45:47 +0100

    new central main view controller

---
+++ src/Cascade/Cascade/CLMainViewController.h
@@ -0,0 +1,17 @@
+//
+//  CLMainViewController.h
+//  TSystems
+//
+//  Created by Thomas Schedler on 01.11.11.
+//  Copyright (c) 2011 MASSIVE ART WebServices. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CLSplitCascadeViewController;
+
+@interface CLMainViewController : UIViewController
+
+@property (nonatomic, strong) CLSplitCascadeViewController* parentSplitViewController;
+
+@end
---
+++ src/Cascade/Cascade/CLMainViewController.m
@@ -0,0 +1,15 @@
+//
+//  CLMainViewController.m
+//  TSystems
+//
+//  Created by Thomas Schedler on 01.11.11.
+//  Copyright (c) 2011 MASSIVE ART WebServices. All rights reserved.
+//
+
+#import "CLMainViewController.h"
+
+@implementation CLMainViewController
+
+@synthesize parentSplitViewController;
+
+@end
